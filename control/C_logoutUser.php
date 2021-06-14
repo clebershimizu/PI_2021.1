42153e0f4ac96e29ad3d261e5d9db4afbb189a15
YurgenSchn <42218362+YurgenSchn@users.noreply.github.com>
2021-06-14 14:58:30 -0300

    Atualização do Logout
    adicionado código para limpar login de admin

--- control/C_logoutUser.php
+++ control/C_logoutUser.php
@@ -3,5 +3,9 @@ session_start();
 unset($_SESSION["loggedUser"]);
 unset($_SESSION["idUser"]);
 unset($_SESSION["nameUser"]);
+
+unset($_SESSION["loggedAdmin"]);
+unset($_SESSION["idAdmin"]);
+
 session_destroy();
 header("Location: ../index.php");
